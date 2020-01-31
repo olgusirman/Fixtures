f5250edc84f5b9fb961f9c95a9238278dde3fa0c
Olgu <olgusirman@gmail.com>
2020-01-31 10:08:21 +0000

    Project is ready for development

---
+++ Fixtures/Base/LaunchManager.swift
@@ -0,0 +1,36 @@
+//
+//  LaunchManager.swift
+//  Fixtures
+//
+//  Created by Olgu on 31.01.2020.
+//  Copyright © 2020 InCrowd Sports. All rights reserved.
+//
+
+import UIKit
+
+final class LaunchManager {
+    
+    var window: UIWindow?
+    
+    required init(window: inout UIWindow?) {
+        self.window = window
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        prepareWindow()
+    }
+    
+    private func prepareWindow() {
+        self.window?.rootViewController = prepareRootController()
+        self.window?.makeKeyAndVisible()
+    }
+    
+    private func prepareRootController() -> UINavigationController {
+        let navigationController = UINavigationController(rootViewController: prepareFixturesController())
+        // According to guideline first pages navigationTitle should be large
+        navigationController.navigationBar.prefersLargeTitles = true
+        return navigationController
+    }
+    
+    private func prepareFixturesController() -> FixturesViewController {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "FixturesViewController") as! FixturesViewController
+    }
+}
--- Fixtures/Scenes/Fixtures/FixturesViewController.swift
+++ Fixtures/Scenes/Fixtures/FixturesViewController.swift
@@ -1,41 +1,18 @@
+//
+//  FixturesViewController.swift
+//  Fixtures
+//
+//  Created by Olgu on 31.01.2020.
+//  Copyright © 2020 InCrowd Sports. All rights reserved.
+//
+
 import UIKit
-import Alamofire
 
-class FixturesViewController: UITableViewController {
-
-    private var matches: [Match]?
+final class FixturesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        Match.fetchMatches {
-            self.matches = $0
-            self.tableView.reloadData()
-        }
+        self.view.backgroundColor = .red
     }
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return matches?.count ?? 0
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "fixture", for: indexPath) as? FixtureTableViewCell,
-            let match = matches?[indexPath.row]
-            else { fatalError() }
-
-        cell.homeTeamNameLabel.text = match.homeTeamName
-        cell.awayTeamNameLabel.text = match.awayTeamName
-        
-        return cell
-    }
-}
-
-class FixtureTableViewCell: UITableViewCell {
-
-    @IBOutlet weak var homeTeamNameLabel: UILabel!
-    @IBOutlet weak var awayTeamNameLabel: UILabel!
 }
